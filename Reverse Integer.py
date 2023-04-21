1c9c7d3de0057489b1469acb8630cb88fef3c423
jshyun1 <shsy1512@gmail.com>
2023-04-21 15:11:54 +0900

    day 4

---
+++ Reverse Integer.py
@@ -0,0 +1,26 @@
+# Reverse Integer.
+# https://leetcode.com/problems/reverse-integer/
+class Solution:
+    def reverse(self, x: int) -> int:
+        # overflow or underflow
+        if x > (2**31 -1) or x < (-2**31):
+            return 0
+
+        # a or b is reverse of x.
+        # and check over / under flow occur.
+        if x >= 0:
+            a = 0
+            while x != 0:
+                a = a * 10 + x % 10
+                x = x // 10
+            if a <= (2**31 - 1):
+                return a
+            return 0
+        else:
+            b = 0
+            while x != 0:
+                b = b * 10 + (x % -10)
+                x = -(x // -10)
+            if b >= (-2**31):
+                return b
+            return 0
